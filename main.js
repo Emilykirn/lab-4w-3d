07864fad60d08e12bba1ba663108c8e2b7d9bc4e
Emily Kirn <emilykirn@gmail.com>
2022-11-09 16:13:00 -0700

    xx

---
+++ main.js
@@ -0,0 +1,21 @@
+let button = document.querySelector('#get-residents')
+
+const buttonClick = () => {
+    let request = axios.get('https://swapi.dev/api/planets/?search=Alderaan')
+    request.then((response) => {
+        let reqArray = response.data.results[0].residents
+        for(let i = 0; i < reqArray.length; i++){
+            let somethingElse = axios.get(reqArray[i]);
+            somethingElse.then((anything) => {
+                let body = document.querySelector('body')
+                let name = anything.data.name 
+                let h2 = document.createElement('h2')
+                h2.textContent = name
+                body.appendChild(h2)
+                // console.log(anything.data.name)
+            })
+        }
+    })
+}
+
+button.addEventListener('click', buttonClick)
